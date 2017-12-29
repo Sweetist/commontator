12be6704a2b717520602aa1147198507d0eca6a8
Alexander Bobrov <al.bobrov.mail@gmail.com>
2017-12-29 09:23:33 +0500

    Replace alias_method_chain with Module#prepend

--- lib/commontator/acts_as_commontable.rb
+++ lib/commontator/acts_as_commontable.rb
@@ -7,32 +7,36 @@ module Commontator
       base.is_commontable = false
       base.extend(ClassMethods)
     end
-    
+
     module ClassMethods
       def acts_as_commontable(options = {})
         class_eval do
           cattr_accessor :commontable_config
+          association_options = options.extract!(:dependent)
           self.commontable_config = Commontator::CommontableConfig.new(options)
           self.is_commontable = true
 
-          has_one :thread, :as => :commontable,
-                           :class_name => 'Commontator::Thread'
+          has_one :thread, as: :commontable,
+                           class_name: 'Commontator::Thread',
+                           dependent: association_options[:dependent]
 
           validates_presence_of :thread
 
-          def thread_with_commontator
-            @thread ||= thread_without_commontator
-            return @thread unless @thread.nil?
-
-            @thread = build_thread
-            @thread.save if persisted?
-            @thread
-          end
-
-          alias_method_chain :thread, :commontator
+          prepend ThreadWithCommontator
         end
       end
-      
+
+      module ThreadWithCommontator
+        def thread
+          @thread ||= super
+          return @thread unless @thread.nil?
+
+          @thread = build_thread
+          @thread.save if persisted?
+          @thread
+        end
+      end
+
       alias_method :acts_as_commentable, :acts_as_commontable
     end
   end
